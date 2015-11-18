af47ff98b7cd24bbd2e9306c0694763af20a01a8
darshan009 <darshansapaliga009@gmail.com>
2015-11-18 23:26:25 +0530

    updated player state

--- js/main.js
+++ js/main.js
@@ -1,79 +1,126 @@
 //temp fn
-/*
-var tile = function(row, column){
-	this.row = row;
-	this.column = column;
+var tile = function(row, column) {
+    this.row = row;
+    this.column = column;
 };
-*/
-var player = function(name, sign){
-	this.sign = sign;
 
-};
-var players =[];
-players[0]= new player(prompt("Enter your player1 name?"));
-players[1] = new player(prompt("Enter your player2 name?"));
-
-playerCounter =0;
-
-var tiles = [];
-//init tiles
-var tile = function(row, column){
-	this.row = row;
-	this.column = column;
-	
-	this.init = function(){
-		
-
-		for(i = 0; i<3; i++){
-		for(j=0; j<3; j++){
-			
-			var tileObject = new tile( i, j);
-			tiles.push(tileObject);
-			console.log("tilepart??");
-			}
-		}
-		return tiles;
-	};
-
-
-	//draw tiles
-	this.drawBoard = function(){
-		for(i=0;i<tiles.length;i++){
-			div = document.createElement('div');
-			div.className = 'tile';
-			div.addEventListener('click', function(event){
-
-					if(player[playerCounter == 0]){
-						drawDiv.innerHTML = '<span onclick="">X</span>';
-						playerCounter++;
-					}
-					else if(player[playerCounter == 1]){
-						drawDiv.innerHTML = '<span onclick="">Y</span>';
-						playerCounter--;
-					}					
-			});
-					console.log("drawpart");
-		}
-		
-		document.body.appendChild(div);
-	};
+var player = function(name, sign) {
+    this.name = name;
+    this.sign = sign;
 
 };
-	var myTiles = new tile(3,3);
-	myTiles.init();
-	myTiles.drawBoard();
 
-	  //testing winner here
-	  var test = function(){
-		 for(i = 0; i<3; i++){
-			for(j=0; j<3; j++){
-			  	if(tile()=="X" && tile()=="X" && tile()=="X"){
 
-		  	}
-		  		if(tile()=="Y" && tile()=="Y" && tile()=="Y"){
-
-	  		}
-	  
-	  }
-}
+var move = function(pos, sign){
+  this.pos = pos;
+  this.sign = sign;
 };
+
+
+var players = [];
+players[0] = new player('Darshan' , 'X');
+players[1] = new player('Kattu' , '0');
+
+playerCounter = 0;
+
+
+//init tiles array
+var board = function() {
+    this.tiles = [];
+
+
+    this.init = function() {
+
+
+        for (i = 0; i < 3; i++) {
+            for (j = 0; j < 3; j++) {
+
+                var tileObject = new tile(i, j);
+                this.tiles.push(tileObject);
+                console.log("tilepart??");
+            }
+        }
+    };
+
+
+    //draw tiles
+    this.drawBoard = function() {
+
+        var boardDiv = document.createElement('div');
+        boardDiv.className = 'board';	
+        boardDiv.setAttribute('id', 'board');
+        
+        // creating seperate row 3*3
+        var row = 0;
+        var rowDiv = [];
+        for (var i = 0; i < 3; i++) {
+            rowDiv[i] = document.createElement('div');
+            rowDiv[i].className = 'row';
+        }
+
+        //for all 9 tiles
+        for (var i = 0; i < this.tiles.length; i++) {
+            var tileDiv = document.createElement('span');
+            tileDiv.className = 'tile';
+            tileDiv.setAttribute('id', 'tile[' + i + ']');
+            tileDiv.setAttribute('data-row', this.tiles[i].row);
+            tileDiv.setAttribute('data-col', this.tiles[i].col);
+
+            rowDiv[row].appendChild(tileDiv);
+            if (i == 2 || i == 5 || i == 8)
+                row++;
+
+
+            tileDiv.addEventListener('click', function(event) {
+                /* lol...
+                					if(player[playerCounter]){
+                						drawDiv.innerHTML = '<span onclick="">X</span>';
+                						playerCounter++;
+                					}
+                					else if(player[playerCounter]){
+                						drawDiv.innerHTML = '<span onclick="">Y</span>';
+                						playerCounter--;
+                					}					
+                  */
+                  this.innerHTML = players[playerCounter].sign;
+                  playerCounter = 1 - playerCounter;
+                  //display turns
+				document.getElementById("messages").innerHTML =players[playerCounter].name+", this is your turn "+playerCounter;
+
+            });
+            console.log("drawpart");
+        }
+
+        this.moves = [];
+        this.moves.push = new move(i, player[playerCounter].sign);
+
+
+        for (i = 0; i < 3; i++) {
+        	//do
+            document.body.appendChild(rowDiv[i]);
+            boardDiv.appendChild(rowDiv[i]);
+        }
+        //do
+        document.body.appendChild(boardDiv);
+    };
+
+   //displaying messages
+  messageDiv = document.createElement('div');
+  messageDiv.setAttribute('id','messages');
+  messageDiv.innerHTML= players[0].name +', this is your turn';
+  document.body.appendChild(messageDiv);
+
+  for(i=0;i<9;i++){
+  	
+	  	if(this.moves[i].sign == this.moves[i+1].sign == this.moves[i+2].sign && this.moves[i+2].pos % 3 == 0){
+	  		document.getElementById("messages").innerHTML =players[playerCounter].name+", you win "+playerCounter;
+	  	}
+  }
+
+
+};
+
+var myBoard = new board();
+myBoard.init();
+myBoard.drawBoard();
+
