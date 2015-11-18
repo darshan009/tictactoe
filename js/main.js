3a651b299193b1ff8fe4ca0b6793e2e4d24a768d
darshan009 <darshansapaliga009@gmail.com>
2015-11-18 12:19:56 +0530

    updated js

--- js/main.js
+++ js/main.js
@@ -3,21 +3,21 @@
 var tile = function(row, column){
 	this.row = row;
 	this.column = column;
-}
+};
 
 var player = function(name, sign){
 	this.sign = sign;
 
-}
+};
 var players =[];
 players[0]= new player(prompt("Enter your player1 name?"));
 players[1] = new player(prompt("Enter your player2 name?"));
 
-
+playerCounter =0;
 
 var tile = function(){
 
-
+	//init tiles
 	this.init = function(){
 		var tiles = [];
 
@@ -27,23 +27,43 @@ var tile = function(){
 			var tileObject = new tile( i, j);
 			tiles.push(tileObject);
 			
+			}
 		}
-	}
-	return tiles;
+		return tiles;
+	};
+	
+	//draw tiles
+	this.drawBoard = function(){
+		for(i=0;i<tiles.length;i++){
+			tileDiv = document.createElement('div');
+			tile.div.className = 'tile';
+			tile.addEventlistener('click', function(event){
 
-	this.drawBoard() = function(){
+					if(player[0])
+						drawTileDiv.innerHTML = '</span><span class="remove" onclick="cartObject.removeFromCart('+i+')">X</span>';
+					else if(player[1])
+						drawTileDiv.innerHTML = '</span><span class="remove" onclick="cartObject.removeFromCart('+i+')">Y</span>';
+					
+			});
+		}
+		document.body.appendChild(tileDiv);
+	};
 
-
+};
+	
 
 	  //testing winner here
 	 for(i = 0; i<3; i++){
 		for(j=0; j<3; j++){
 			
-		  function test(){
+		  var test = function(){
 	  	if(tile(1,1)=="X" && tile(i+i,j+1)=="X" && tile()=="X"){
 
 	  	}
-	  		if(tile(1,1)=="Y" && tile(i+i,j+1)=="Y" && tile()=="Y")
+	  		if(tile(1,1)=="Y" && tile(i+i,j+1)=="Y" && tile()=="Y"){
+
+	  		}
 	  
-	  }
+	  };
+}
 }
